c1e8f66c5683fdc700e08b2f020a1a29283f1385
shivanshi <shivanshi.saxena2@gmail.com>
2024-07-30 18:16:29 +0530

    Sticky Notes App - Basic Setup

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,50 @@
+import { useRef, useEffect } from "react";
+import Trash from "../icons/Trash";
+
+const NoteCard = ( {note} ) => {
+    let position = JSON.parse(note.position);
+    const colors  = JSON.parse(note.colors);
+    const body = JSON.parse(note.body);
+
+    const textAreaRef = useRef(null)
+
+    useEffect(() => {
+        autoGrow(textAreaRef)
+    },[]);
+
+    const autoGrow = (textarea) => {
+        const {current} = textAreaRef
+        current.style.height = "auto"; // Reset the height
+        current.style.height = current.scrollHeight + "px"; // Set the new height
+    }
+
+  return (
+    <div className="card"
+        style={{backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        }}
+    >
+
+        <div className="card-body">
+            <textarea
+                ref={textAreaRef}
+                style={{ color: colors.colorText }}
+                defaultValue={body}
+                onInput={() => {autoGrow(textAreaRef)}}
+            ></textarea>
+        </div>
+
+        <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+        >
+            <Trash/>
+        </div>
+ 
+        <div className="card-body"></div>
+    </div>
+    
+  )
+}
+export default NoteCard
---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,13 @@
+import {fakeData as notes} from "../assets/fakeData.js"
+import NoteCard from "../components/NoteCard.jsx"
+
+const NotesPage = () => {
+  return (
+    <div>
+        {notes.map((note) => (
+            <NoteCard key={note.$id} note={note}/>
+        ))}
+    </div>
+  )
+}
+export default NotesPage
